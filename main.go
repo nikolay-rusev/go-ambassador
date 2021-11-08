147674752b536bf758dabdb1bb157ce226e9a9ae
n.rusev <n.rusev@tinqin.com>
2021-11-08 16:43:40 +0200

    add jwt, cookie & cors

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 
     app := fiber.New()
 
+    app.Use(cors.New(cors.Config{
+       AllowCredentials: true
+    }))
+
     routes.Setup(app)
 
     log.Fatal(app.Listen(":8000"))
--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -3,8 +3,11 @@ package controllers
 import (
     "ambassador/src/database"
     "ambassador/src/models"
+    "github.com/dgrijalva/jwt-go"
     "github.com/gofiber/fiber/v2"
     "golang.org/x/crypto/bcrypt"
+    "strconv"
+    "time"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -61,5 +64,30 @@ func Login(c *fiber.Ctx) error {
         })
     }
 
-    return c.JSON(user)
+    payload := jwt.StandardClaims{
+        Subject: strconv.Itoa(int(user.Id)),
+        ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+    }
+
+    token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+
+    if err != nil {
+        c.Status(fiber.StatusBadRequest)
+        return c.JSON(fiber.Map{
+            "message": "Invalid credentials!",
+        })
+    }
+
+    cookie := fiber.Cookie{
+        Name: "jwt",
+        Value: token,
+        Expires: time.Now().Add(time.Hour * 24),
+        HTTPOnly: true,
+    }
+
+    c.Cookie(&cookie)
+
+    return c.JSON(fiber.Map{
+        "message": "success",
+    })
 }
