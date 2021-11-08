ef5340e0db82cdcf5bfc7dac2c057d5fdef39696
n.rusev <n.rusev@tinqin.com>
2021-11-08 12:40:27 +0200

    add db and connect

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
     "log"
-
     "github.com/gofiber/fiber/v2"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 )
 
 func main() {
+    _, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+    if err != nil {
+        panic("Could not connect with the database!")
+    }
+
     app := fiber.New()
 
     app.Get("/", func (c *fiber.Ctx) error {
